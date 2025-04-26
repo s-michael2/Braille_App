84f9e91f1c6ae65000ef9849688e60f4c1b79901
s-michael2 <112111422+s-michael2@users.noreply.github.com>
2025-04-26 09:54:39 -0400

    Add files via upload

---
+++ Braille_App_3/Braille_App_3/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ConnectionPanel = New System.Windows.Forms.FlowLayoutPanel()
+        Me.ConnectionTitle = New System.Windows.Forms.Label()
+        Me.ButtonSend = New System.Windows.Forms.Button()
+        Me.ComboBoxPorts = New System.Windows.Forms.ComboBox()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.ButtonDisconnect = New System.Windows.Forms.Button()
+        Me.LabelStatus = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TimerSerial = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.TextBoxInput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ConnectionPanel
+        '
+        Me.ConnectionPanel.BackColor = System.Drawing.Color.White
+        Me.ConnectionPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ConnectionPanel.Location = New System.Drawing.Point(0, 0)
+        Me.ConnectionPanel.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ConnectionPanel.Name = "ConnectionPanel"
+        Me.ConnectionPanel.Size = New System.Drawing.Size(1200, 692)
+        Me.ConnectionPanel.TabIndex = 0
+        '
+        'ConnectionTitle
+        '
+        Me.ConnectionTitle.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ConnectionTitle.AutoSize = True
+        Me.ConnectionTitle.BackColor = System.Drawing.Color.White
+        Me.ConnectionTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ConnectionTitle.ForeColor = System.Drawing.Color.Black
+        Me.ConnectionTitle.Location = New System.Drawing.Point(36, 89)
+        Me.ConnectionTitle.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.ConnectionTitle.Name = "ConnectionTitle"
+        Me.ConnectionTitle.Size = New System.Drawing.Size(307, 22)
+        Me.ConnectionTitle.TabIndex = 0
+        Me.ConnectionTitle.Text = "Select port and then hit connect :"
+        Me.ConnectionTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ButtonSend
+        '
+        Me.ButtonSend.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ButtonSend.AutoSize = True
+        Me.ButtonSend.BackColor = System.Drawing.Color.Blue
+        Me.ButtonSend.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.ButtonSend.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ButtonSend.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ButtonSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonSend.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSend.ForeColor = System.Drawing.Color.White
+        Me.ButtonSend.Location = New System.Drawing.Point(952, 352)
+        Me.ButtonSend.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ButtonSend.Name = "ButtonSend"
+        Me.ButtonSend.Size = New System.Drawing.Size(112, 38)
+        Me.ButtonSend.TabIndex = 0
+        Me.ButtonSend.Text = "Send"
+        Me.ButtonSend.UseVisualStyleBackColor = False
+        '
+        'ComboBoxPorts
+        '
+        Me.ComboBoxPorts.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ComboBoxPorts.BackColor = System.Drawing.Color.Gray
+        Me.ComboBoxPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxPorts.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ComboBoxPorts.ForeColor = System.Drawing.Color.White
+        Me.ComboBoxPorts.FormattingEnabled = True
+        Me.ComboBoxPorts.Location = New System.Drawing.Point(368, 85)
+        Me.ComboBoxPorts.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ComboBoxPorts.Name = "ComboBoxPorts"
+        Me.ComboBoxPorts.Size = New System.Drawing.Size(156, 28)
+        Me.ComboBoxPorts.TabIndex = 0
+        '
+        'ButtonConnect
+        '
+        Me.ButtonConnect.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ButtonConnect.AutoSize = True
+        Me.ButtonConnect.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(120, Byte), Integer))
+        Me.ButtonConnect.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.ButtonConnect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ButtonConnect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ButtonConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonConnect.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonConnect.ForeColor = System.Drawing.Color.White
+        Me.ButtonConnect.Location = New System.Drawing.Point(540, 85)
+        Me.ButtonConnect.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(110, 38)
+        Me.ButtonConnect.TabIndex = 2
+        Me.ButtonConnect.Text = "Connect"
+        Me.ButtonConnect.UseVisualStyleBackColor = False
+        '
+        'ButtonDisconnect
+        '
+        Me.ButtonDisconnect.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ButtonDisconnect.AutoSize = True
+        Me.ButtonDisconnect.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.ButtonDisconnect.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.ButtonDisconnect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(200, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.ButtonDisconnect.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(228, Byte), Integer), CType(CType(90, Byte), Integer), CType(CType(90, Byte), Integer))
+        Me.ButtonDisconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonDisconnect.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDisconnect.ForeColor = System.Drawing.Color.White
+        Me.ButtonDisconnect.Location = New System.Drawing.Point(60, 209)
+        Me.ButtonDisconnect.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ButtonDisconnect.Name = "ButtonDisconnect"
+        Me.ButtonDisconnect.Size = New System.Drawing.Size(219, 38)
+        Me.ButtonDisconnect.TabIndex = 3
+        Me.ButtonDisconnect.Text = "Disconnect"
+        Me.ButtonDisconnect.UseVisualStyleBackColor = False
+        '
+        'LabelStatus
+        '
+        Me.LabelStatus.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.LabelStatus.AutoSize = True
+        Me.LabelStatus.BackColor = System.Drawing.Color.White
+        Me.LabelStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelStatus.ForeColor = System.Drawing.Color.Red
+        Me.LabelStatus.Location = New System.Drawing.Point(30, 618)
+        Me.LabelStatus.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LabelStatus.Name = "LabelStatus"
+        Me.LabelStatus.Size = New System.Drawing.Size(283, 32)
+        Me.LabelStatus.TabIndex = 4
+        Me.LabelStatus.Text = "Status: Disconnected"
+        '
+        'TextBoxInput
+        '
+        Me.TextBoxInput.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.TextBoxInput.Location = New System.Drawing.Point(368, 355)
+        Me.TextBoxInput.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBoxInput.Name = "TextBoxInput"
+        Me.TextBoxInput.Size = New System.Drawing.Size(564, 26)
+        Me.TextBoxInput.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(34, 358)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(310, 22)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "     Type letter and then hit send :"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1200, 692)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxInput)
+        Me.Controls.Add(Me.LabelStatus)
+        Me.Controls.Add(Me.ButtonConnect)
+        Me.Controls.Add(Me.ComboBoxPorts)
+        Me.Controls.Add(Me.ButtonSend)
+        Me.Controls.Add(Me.ConnectionTitle)
+        Me.Controls.Add(Me.ConnectionPanel)
+        Me.Controls.Add(Me.ButtonDisconnect)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ConnectionPanel As FlowLayoutPanel
+    Friend WithEvents ConnectionTitle As Label
+    Friend WithEvents ButtonSend As Button
+    Friend WithEvents ComboBoxPorts As ComboBox
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents ButtonDisconnect As Button
+    Friend WithEvents LabelStatus As Label
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents TimerSerial As Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TextBoxInput As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Braille_App_3/Braille_App_3/Form1.vb
@@ -0,0 +1,150 @@
+﻿Imports System.IO.Ports
+Imports System.Media ' Required for playing audio
+
+Public Class Form1
+    Private WithEvents _serialPort As SerialPort
+    Private _isConnected As Boolean = False ' Tracks the connection state
+
+    ' Load available COM ports on form load
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadAvailablePorts()
+        ButtonSend.Enabled = False ' Disable Send button initially
+    End Sub
+
+    Private Sub LoadAvailablePorts()
+        Try
+            Dim ports As String() = SerialPort.GetPortNames()
+            ComboBoxPorts.Items.Clear()
+            ComboBoxPorts.Items.AddRange(ports)
+            If ports.Length > 0 Then
+                ComboBoxPorts.SelectedIndex = 0
+            Else
+                MessageBox.Show("No COM ports available. Please connect your Arduino and restart the app.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Catch ex As Exception
+            MessageBox.Show($"Error loading COM ports: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Toggle connect/disconnect
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        If Not _isConnected Then
+            ' Attempt to connect
+            If ComboBoxPorts.SelectedItem Is Nothing Then
+                MessageBox.Show("Please select a COM port.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            Try
+                _serialPort = New SerialPort(ComboBoxPorts.SelectedItem.ToString(), 9600) With {
+                    .NewLine = vbCrLf
+                }
+                _serialPort.Open()
+                LabelStatus.Text = "Status: Connected"
+                LabelStatus.ForeColor = Color.Green
+                ButtonConnect.Text = "Disconnect" ' Change button text
+                ButtonConnect.BackColor = Color.Red
+                ButtonSend.Enabled = True ' Enable Send button
+                _isConnected = True ' Update connection state
+            Catch ex As UnauthorizedAccessException
+                MessageBox.Show("Access to the COM port is denied. Please close any other applications using this port.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Catch ex As Exception
+                MessageBox.Show($"Failed to connect: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Else
+            ' Attempt to disconnect
+            Try
+                If _serialPort IsNot Nothing AndAlso _serialPort.IsOpen Then
+                    _serialPort.Close()
+                    LabelStatus.Text = "Status: Disconnected"
+                    LabelStatus.ForeColor = Color.Red
+                    ButtonConnect.Text = "Connect" ' Change button text
+                    ButtonSend.Enabled = False ' Disable Send button
+                    _isConnected = False ' Update connection state
+                Else
+                    MessageBox.Show("No active connection to disconnect.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Catch ex As Exception
+                MessageBox.Show($"Failed to disconnect: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    ' Send input to Arduino and play sound
+    Private Sub ButtonSend_Click(sender As Object, e As EventArgs) Handles ButtonSend.Click
+        Dim input As String = TextBoxInput.Text.Trim()
+        If String.IsNullOrEmpty(input) Then
+            MessageBox.Show("Please enter text to send.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        SendCommand(input)
+        PlaySound(input) ' Play corresponding sound for the input
+    End Sub
+
+    ' Send data to Arduino
+    ' Send data for each character and update status dynamically
+    Private Sub SendCommand(input As String)
+        Try
+            ' Iterate through each character in the input string
+            For Each character As Char In input
+                ' Ensure serial port is open before sending
+                If _serialPort IsNot Nothing AndAlso _serialPort.IsOpen Then
+                    _serialPort.WriteLine(character.ToString()) ' Send the character to Arduino
+                    LabelStatus.Text = $"Sent: {character}" ' Update status for each character
+                    Application.DoEvents() ' Refresh UI immediately to reflect changes
+                    'System.Threading.Thread.Sleep(500) ' Optional delay for each character
+
+                Else
+                    MessageBox.Show("Not connected to Arduino. Please connect first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Exit For ' Stop processing if disconnected
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show($"Failed to send command: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Play sound for each character in the input string with a delay and special handling for capital letters
+    Private Sub PlaySound(input As String)
+        Try
+            ' Iterate through each character in the input string
+            For Each character As Char In input
+                ' Add a delay before starting the audio
+                System.Threading.Thread.Sleep(1500) ' 1500 milliseconds delay
+
+                ' Play a special sound if the character is a capital letter
+                If Char.IsUpper(character) Then
+                    Dim capitalSound As String = "C:\Users\smich\source\repos\Braille_App_3\Sounds\capital.wav"
+                    If IO.File.Exists(capitalSound) Then
+                        Dim player As New SoundPlayer(capitalSound)
+                        player.Play() ' Play synchronously to ensure order
+                    End If
+                End If
+
+                ' Play sound for the individual character
+                Dim soundFile As String = $"C:\Users\smich\source\repos\Braille_App_3\Sounds\{Char.ToLower(character)}.wav" ' Convert to lowercase for file naming
+                If IO.File.Exists(soundFile) Then
+                    Dim player As New SoundPlayer(soundFile)
+                    player.PlaySync() ' Play synchronously to ensure order
+                Else
+                    ' Play a default sound if no specific file exists
+                    Dim defaultSound As String = "C:\Users\smich\source\repos\Braille_App_3\Sounds\default.wav"
+                    If IO.File.Exists(defaultSound) Then
+                        Dim player As New SoundPlayer(defaultSound)
+                        player.Play() ' Play synchronously to ensure order
+                    End If
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show($"Failed to play sound: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Close serial port on form closing
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If _serialPort IsNot Nothing AndAlso _serialPort.IsOpen Then
+            _serialPort.Close()
+        End If
+    End Sub
+End Class
